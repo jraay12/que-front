5dd24593a6a9bb0344ff677b1c5f4ed0031c1629
John Ray Canete <johnraycaete@gmail.com>
2023-05-09 00:19:28 +0800

    First Commit

---
+++ que/src/App.js
@@ -0,0 +1,23 @@
+import Login from "./Pages/Auth/Login";
+import Faculty from "./Pages/Dashboard/Faculty";
+import GuestStudent from "./Pages/Dashboard/GuestStudent";
+
+import {Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route path="/Faculty" element={<Faculty/>}>
+
+        </Route>
+
+        <Route path="/Guest" element={<GuestStudent/>} >
+
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ que/src/Pages/Dashboard/GuestStudent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GuestStudent = () => {
+  return (
+    <div>GuestStudent</div>
+  )
+}
+
+export default GuestStudent
