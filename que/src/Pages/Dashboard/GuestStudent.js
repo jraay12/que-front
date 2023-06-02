65657ab6b36b14fe1dffeb2c4d514b6b9519e3bd
John Ray <johnraycaete@gmail.com>
2023-06-03 00:21:19 +0800

    Done with Add COntainter

--- que/src/Pages/Dashboard/GuestStudent.js
+++ que/src/Pages/Dashboard/GuestStudent.js
@@ -25,9 +25,9 @@ const GuestStudent = () => {
     
   }
   return (
-      <div className='flex flex-col min-h-screen bg-background object-cover w-screen bg-black'>
+      <div className='flex flex-col min-h-screen bg-background object-cover w-screen  bg-black'>
         <div className='flex justify-end w-screen h-14' >
-          <div className='text-white flex justify-center  gap-5 items-center mx-10 font-semibold'>
+          <div className='text-white flex justify-center gap-5 items-center mx-10 font-semibold'>
             <div className='flex flex-col items-center leading-3 text-2xl hover:cursor-pointer' > 
             <div className='flex' onClick={toggleOpen}>
               <BsQrCodeScan />
--- que/src/components/AddContainer.js
+++ que/src/components/AddContainer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {AiOutlineClose} from "react-icons/ai"
 import Button from './Button'
+import { useNavigate } from 'react-router-dom'
 const AddContainer = () => {
+  const navigate = useNavigate();
   return (
-    <div className='flex justify-center items-center h-screen w-screen absolute'>
+    <div className='flex justify-center items-center h-screen w-screen absolute backdrop-blur-sm'>
         <div className='min-w-[350px] min-h-[200px] bg-white rounded-2xl'>
-        <div className='hover:cursor-pointer flex justify-end mx-4 mt-2'>
+        <div className='hover:cursor-pointer flex justify-end mx-4 mt-2' onClick={() => navigate("/Dashboard")}>
               <AiOutlineClose />
             </div>
           <div className='flex flex-col gap-4 justify-center items-center'>
@@ -13,13 +15,13 @@ const AddContainer = () => {
           <h1  className='text-blue text-2xl font-medium'>I'm a ________ .</h1>
           </div>
           <div className='flex justify-evenly '>
-            <div className=' text-white py-2 mt-4 rounded-2xl bg-blue w-[100px] flex justify-center'>
+            <div className=' text-white font-bold py-2 mt-4 rounded-2xl hover:opacity-70 bg-blue w-[100px] flex justify-center'>
             <Button 
             buttonName = "Guest"
             type= "submit"
             />
             </div>
-            <div className='text-white py-2 mt-4 rounded-2xl bg-blue w-[100px] flex justify-center'>
+            <div className='text-white font-bold py-2 mt-4 rounded-2xl hover:opacity-70 bg-blue w-[100px] flex justify-center'>
             <Button 
             buttonName = "Student"
             type= "submit"
