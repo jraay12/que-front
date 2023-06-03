a23b7e4019af008cf4fb73364d6c3365102b385d
Willy Jean Balanay <willyjean112001@gmail.com>
2023-06-03 23:31:39 +0700

    Add folder to repository

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CreateQue from './components/CreateQue';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header"> */}
+      {/* </header> */}
+      <CreateQue />
+    </div>
+  );
+}
+
+export default App;
