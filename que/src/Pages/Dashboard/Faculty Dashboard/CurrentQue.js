578e7e04fee0fbd68e9361fb68618748af0be9ee
John Ray <johnraycaete@gmail.com>
2023-06-15 22:07:59 +0800

    Add information card for current queue

--- que/src/Pages/Dashboard/Faculty Dashboard/CurrentQue.js
+++ que/src/Pages/Dashboard/Faculty Dashboard/CurrentQue.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { currentData } from "../../../SampleData/SampleData";
+import { Outlet, useNavigate } from "react-router-dom";
 const CurrentQue = () => {
+
+  const navigate = useNavigate()
   return (
     <div className="flex w-full bg-no-repeat bg-cover justify-center items-center min-h-screen bg-background backdrop-blur-lg">
       <div className="drop-shadow shadow-2xl bg-opacity-60 shadow-black backdrop-blur-sm rounded-3xl w-full max-h-[70%] overflow-auto bg-white min-h-[70%] mx-32">
@@ -16,7 +19,7 @@ const CurrentQue = () => {
           <tbody class="flex flex-col items-center justify-between h-[50%] overflow-auto w-full ">
             {Array.isArray(currentData) &&
               currentData.map((item) => (
-                <tr class="flex w-full mb-4 hover:bg-gray-500 hover:cursor-pointer">
+                <tr class="flex w-full mb-4 hover:bg-gray-500 hover:cursor-pointer" onClick={() => navigate("/Faculty/CurrentQue/Information")}>
                   <td class="p-4 w-1/4">{item.Queue}</td>
                   <td class="p-4 w-1/4">{item.Identity}</td>
                   <td class="p-4 w-1/4">{item.Name}</td>
@@ -26,6 +29,7 @@ const CurrentQue = () => {
           </tbody>
         </table>
       </div>
+      <Outlet />
     </div>
   );
 };
