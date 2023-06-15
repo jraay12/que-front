0e76202e2fd4ecd229280cfd23d2e8516c61f06e
John Ray <johnraycaete@gmail.com>
2023-06-15 14:22:39 +0800

    Add sidebar in faculty

--- que/src/Pages/Dashboard/Faculty.js
+++ que/src/Pages/Dashboard/Faculty.js
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import Sidebar from "../../components/Sidebar";
+import { Outlet } from "react-router-dom";
 const Faculty = () => {
   return (
-    <div>Faculty</div>
-  )
-}
+    <div className="flex max-w-max min-w-max min-h-screen">
+        <Sidebar />
+        <Outlet />
+    </div>
+  );
+};
 
-export default Faculty
+export default Faculty;
