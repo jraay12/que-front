9b16ad2bcb85e95c596231fd2951e867ec7ab653
John Ray <johnraycaete@gmail.com>
2023-06-15 13:15:55 +0800

    Made same Changes in AddQueFormStudent

--- que/src/components/AddQueFormStudent.js
+++ que/src/components/AddQueFormStudent.js
@@ -7,26 +7,20 @@ const AddQueFormStudent = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-center items-center w-screen h-screen bg-background backdrop-blur-lg absolute ">
+    <div className="flex justify-center items-center w-screen h-screen bg-cover bg-no-repeat bg-background backdrop-blur-lg absolute ">
       <div className="container bg-slate-300 flex flex-col xxl:h-3/6 shadow-2xl drop-shadow-2xl xxl:w-[30%] h-[90%] xl:h-[70%] w-[40%] rounded-3xl">
         <div className="flex mt-10 items-center flex-col">
           <h1 className="font-extrabold md:text-lg lg:text-2xl xl:text-4xl ">
             Add Queue
           </h1>
         </div>
-        <div className="flex justify-start  md:mt-10 mx-10">
+        <div className="w-full px-10 flex flex-col gap-4 mt-6">
           <Input label="Student Number" py={3} />
-        </div>
-        <div className="flex justify-start mt-2 mx-10">
           <Input label="Student Name" py={3} />
-        </div>
-        <div className="flex justify-start  mt-4 mx-10">
           <Input label="Student Email" py={3} />
-        </div>
-        <div className="flex justify-start  h-36 mt-4 mx-10 leading-none">
           <Input label="Purpose" py={3} />
         </div>
-        <div className="flex justify-evenly gap-4 leading-none -mt-10 xxl:mt-10">
+        <div className="flex justify-evenly gap-4 leading-none mt-6  xxl:mt-10">
           <div className="bg-blue hover:bg-red-600 text-white rounded-2xl h-10 w-20 flex justify-center items-center">
             <Button
               buttonName="Cancel"
