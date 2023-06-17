59d3a94b214172babfee98835ed2400186a2a396
John Ray <johnraycaete@gmail.com>
2023-06-17 22:19:02 +0800

    Fix the responsiveness in Guest/Student

--- que/src/components/Container.js
+++ que/src/components/Container.js
@@ -3,10 +3,10 @@ import React from 'react'
 
 const Container = (props) => {
   return (
-    <div className='min-w-[100px] container bg-white text-blue text-sm min-h-[70%] xl:pt-32 xxl:text-4xl flex-auto rounded-2xl flex gap-10 flex-col items-center text-left pt-5 hover:cursor-pointer' onClick={props.onClick}>
+    <div className='max-w-[20%] xxl:max-w-[30%] container bg-white text-blue text-sm min-h-[60%] xxl:min-h-[50%] xl:pt-32 xxl:text-4xl flex-auto rounded-2xl flex gap-10 flex-col items-center text-left pt-5 hover:cursor-pointer' onClick={props.onClick}>
         <img src={props.image}/>
         <div className='flex flex-col justify-start'>
-        <h1 className='font-bold text-lg '>{props.name}</h1>
+        <h1 className='font-bold text-lg xxl:text-4xl '>{props.name}</h1>
         <h1 className=' text-md font-serif '><span className='font-bold'>Position</span> : {props.position}</h1>
         </div>
     </div>  
